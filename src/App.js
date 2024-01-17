8db859be6257b011a3f18d347c22909f128d2316
cintamg <76482225+cintamg@users.noreply.github.com>
2024-01-17 12:51:09 -0800

    news category

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import "./sidebar.css";
+import {
+  BrouserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <Router>
+      <Navbar/>
+      <Sidebar/>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Journal/>}></Route>
+        </Routes>
+      </div>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+      <li><a href="/">Wall Street Journal</a></li>
+        <li><a href="apple">Apple News</a></li>
+        <li><a href="/tesla">Tesla News</a></li>
+        <li><a href="/business">Business News</a></li>
+        <li><a href="/tech">TechCrunc</a></li>
+      </ul>
+    </div>
+  )
+}
