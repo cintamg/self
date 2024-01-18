027482b49e3ae917a3f0fe45cc3a8bd636bea3b6
cintamg <76482225+cintamg@users.noreply.github.com>
2024-01-18 11:16:38 -0800

    sidebar complete

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,11 +4,11 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <dl className='my-3'>
-        <di><a href="/journal">Wall Street Journal</a></di><br/>
-        <di><a href="apple">Apple News</a></di><br/>
-        <di><a href="/tesla">Tesla News</a></di><br/>
-        <di><a href="/business">Business News</a></di><br/>
-        <di><a href="/tech">TechCrunc</a></di>
+        <dt><a href="/journal">Wall Street Journal</a></dt><br/>
+        <dt><a href="apple">Apple News</a></dt><br/>
+        <dt><a href="/tesla">Tesla News</a></dt><br/>
+        <dt><a href="/business">Business News</a></dt><br/>
+        <dt><a href="/tech">TechCrunc</a></dt>
       </dl>
     </div>
   )
