60190d9a9c92ee066b3a2f98ace0c9d86e39b593
cintamg <76482225+cintamg@users.noreply.github.com>
2024-01-18 16:34:46 -0800

    slides

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
         '/images/image2.png',
         '/images/image3.png',
         '/images/image4.png',
-        '/images/image5.png',
+        '/images/image5.png'
     ]
 
     const totalSlides = images.length;
@@ -17,23 +17,23 @@ export default function Home() {
     const showSlide = (index) => {
         if(index < 0)
         setCurrSlide(totalSlides-1)
-        else if(index > totalSlides)
+        else if(index >= totalSlides)
         setCurrSlide(0)
         else
         setCurrSlide(index)
-    }
+    };
 
     const nextSlide = () => {
         showSlide(currSlide+1)
-    }
+    };
 
     const prevSlide = () => {
         showSlide(currSlide-1)
-    }
+    };
 
     useEffect(() => {
         // Automatically advance slides every 10 seconds
-        const intervalId = setInterval(nextSlide, 3000);
+        const intervalId = setInterval(nextSlide, 4000);
         // Clear the interval when the component is unmounted
         return () => clearInterval(intervalId);
     },[currSlide]);
@@ -42,14 +42,14 @@ export default function Home() {
     <div className="home">
         <h1 className='text-center'><b>Today's News</b></h1>
         <div className="slider-container">
-        <div className="slides">
+        <div className="slides" style={{ transform: `translateX(${-currSlide * 100}%)` }}>
             {images.map((image, index) => (
             <div key={index} className="slide">
+                {console.log(image)}
                 <img src={image} alt={`Slide ${index + 1}`} />
             </div>
             ))}
         </div>
-
         <div className="arrow prev" onClick={prevSlide}>&#8249;</div>
         <div className="arrow next" onClick={nextSlide}>&#8250;</div>
         </div>
